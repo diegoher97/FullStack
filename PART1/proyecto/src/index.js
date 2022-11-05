72070368623b7709ce5ae9375408e2b1bf525a8b
Diego Alejandro Hernandez Villegas <diego.hernandez@gtdcolombia.local>
2022-11-05 12:01:38 -0500

    Fin Ejercicio 1

--- PART1/proyecto/src/index.js
+++ PART1/proyecto/src/index.js
@@ -1,5 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+//Componente Header
+const Header =(promps)=>(
+  <h1>{promps.NameCourse}</h1>
+)
+//Componente Content
+const Content=(promps)=>(
+<div>
+  <Part part={promps.part1} exercises={promps.exercises1}/>
+  <Part part={promps.part2} exercises={promps.exercises2}/>
+  <Part part={promps.part3} exercises={promps.exercises3}/>
+</div>
+)
+//Componente Total
+const Total =(promps)=>(
+  <p>Number of exercises promps {promps.total}</p>
+)
+
+//Componente Parte
+const Part =(promps)=>(
+  <p>
+    {promps.part} {promps.exercises}
+  </p>
+)
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -9,20 +32,12 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+ 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header NameCourse={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/> 
+      <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
